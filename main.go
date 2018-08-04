c71865d167a49a3095cc5bf44a2118ae146397e1
諦念 <teinen.github@gmail.com>
2018-08-04 14:50:45 +0900

    Add People/Address struct

--- main.go
+++ main.go
@@ -7,6 +7,20 @@ import (
 	"github.com/gorilla/mux"
 )
 
+/** Person struct */
+type Person struct {
+	ID string `json:"id,omitempty"`
+	Firstname string `json:"firstname,omitempty"`
+	Lastname string `json:"lastname,omitempty"`
+	Address *Address `json:"address,omitempty"`
+}
+
+/** Address struct */
+type Address struct {
+	City string `json:"city,omitempty"`
+	State string `json:"state,omitempty"`
+}
+
 // main function
 func main() {
 	// create router
